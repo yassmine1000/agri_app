f266f18db9327b12f6aedac034620b531c7366ee
Yassmine <https://github.com/yassmine1000>
2026-04-26 00:02:56 +0100

    modification arabe

--- smart-agriculture-flutter-app-frontend-main/lib/config.dart
+++ smart-agriculture-flutter-app-frontend-main/lib/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const String serverUrl = 'https://mazier-unschooled-racheal.ngrok-free.dev';
+  static const String serverUrl = 'https://agri-app-5pab.onrender.com';
   static const String baseUrl = '$serverUrl/api';
   static const String agmarknetAPIKey = 'AGMARKNET_API_KEY';
 }
